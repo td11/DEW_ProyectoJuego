631a1f60719f0bf5b2bf52e8ff3bc07129ef35d6
td11 <33686827+td11@users.noreply.github.com>
2018-01-25 11:35:55 +0000

    portatil
    mejora de login

--- JS/login.js
+++ JS/login.js
@@ -1,2 +1,28 @@
-var nombreusuario = "",
-    contraseña = "";
+// Name and Password from the register-form
+var nombre = document.getElementById('userName');
+var password = document.getElementById('userPassword');
+
+// storing input from register-form
+function store() {
+    localStorage.setItem('name', name.value);
+    localStorage.setItem('pw', pw.value);
+}
+
+// check if stored data from register-form is equal to entered data in the   login-form
+function check() {
+
+    // stored data from the register-form
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    // entered data from the login-form
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+
+    // check if stored data from register-form is equal to data from login form
+    if(userName.value !== storedName || userPw.value !== storedPw) {
+        alert('ERROR');
+    }else {
+        alert('You are loged in.');
+    }
+}
